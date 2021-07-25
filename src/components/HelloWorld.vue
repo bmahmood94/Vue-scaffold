be773da1991b874c6cde44c88bbe18eac06d86aa
DESKTOP-8F1H2QJ\bmmah <belal.mahmood561@gmail.com>
2021-07-25 12:54:26 -0400

    commit 1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,13 @@
+
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
+
+    <section>
+      <input v-model="message" placeholder="edit me">
+      <p>Message is: {{ message }}</p>
+    </section>
+   <!-- <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
@@ -27,6 +33,7 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    -->
   </div>
 </template>
 
